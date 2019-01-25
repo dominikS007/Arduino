68aa955e1a8e903518adbd37aaf9414c6cc7790f
Dominik Sienczuk <32140258+dominikS007@users.noreply.github.com>
2019-01-25 01:15:38 +0000

    Update tempLcd.ino

--- tempLcd/tempLcd.ino
+++ tempLcd/tempLcd.ino
@@ -1,28 +1,43 @@
-  GNU nano 3.2                      tempCalc.ino                      
-Modified
+#include<LiquidCrystal.h> 
 
-include <LiquidCrystal.h>
-LiquidCrystal lcd(0, 1, 5, 4, 3, 2);
-const int sensorPin = A0;
-const float baselineTemp = 30.0;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 
-void setup()
+const int temperaturePin = 0;
+
+void setup() 
 {
-
-  Serial.begin(9600);   // opens serial port, sets data rate to 9600 bps
-
+  lcd.begin(16, 2); 
+  pinMode(9, OUTPUT); //set pin 9 to OUTPUT
+  pinMode(10, OUTPUT); //Set pin 10 to OUTPUT
+  digitalWrite(9, LOW); //outputs no power to the contrast pin this lets you see the words
+  analogWrite(10, 255);	//Outputs full power to the screen brightness LED
+  Serial.begin(9600);
+  
 }
-
 void loop()
 {
+  lcd.setCursor(0, 0);
 
-  int sensorVal = analogRead(sensorPin);
-  Serial.print("Sensor Value: ");
-  Serial.print(sensorVal);
+  float voltage, degreesC, degreesF;  
 
-                 [ Read 33 lines (Converted from DOS format) ]
-^G Get Help  ^O Write Out ^W Where Is  ^K Cut Text  ^J Justify   ^C Cur 
-Pos
-^X Exit      ^R Read File ^\ Replace   ^U Uncut Text^T To Spell  ^_ Go 
-To Line
+  voltage = getVoltage(temperaturePin);  
+  degreesC = (voltage - 0.5) * 100.0;
+  degreesF = degreesC * (9.0/5.0) + 32.0;
 
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.print("  deg C: ");
+  Serial.print(degreesC);
+  Serial.print("  deg F: ");
+  Serial.println(degreesF);
+  delay(500);
+  
+  lcd.print("Temp :");
+  lcd.print(" ");
+  lcd.print(degreesC);
+}
+
+float getVoltage(int pin)
+{
+  return (analogRead(pin) * 0.004882814);
+}
