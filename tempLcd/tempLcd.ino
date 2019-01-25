a869e0f4a0c3cac42a9ab338170d21f4aaf3df9b
Dominik Sienczuk <32140258+dominikS007@users.noreply.github.com>
2019-01-25 17:31:07 +0000

    Update tempLcd.ino

--- tempLcd/tempLcd.ino
+++ tempLcd/tempLcd.ino
@@ -1,4 +1,4 @@
-#include<LiquidCrystal.h> 
+#include <LiquidCrystal.h> 
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 
@@ -20,8 +20,10 @@ void loop()
 
   float voltage, degreesC, degreesF;  
 
-  voltage = getVoltage(temperaturePin);  
+  voltage = getVoltage(temperaturePin);
+  
   degreesC = (voltage - 0.5) * 100.0;
+
   degreesF = degreesC * (9.0/5.0) + 32.0;
 
   Serial.print("voltage: ");
@@ -35,6 +37,7 @@ void loop()
   lcd.print("Temp :");
   lcd.print(" ");
   lcd.print(degreesC);
+ 
 }
 
 float getVoltage(int pin)
