3b0913250ad6dc196f6206747809f05dda59ed4a
Dominik Sienczuk <32140258+dominikS007@users.noreply.github.com>
2019-01-25 09:29:39 +0000

    Update movingServo.ino
    fixed errors

--- moodCue/movingServo.ino
+++ moodCue/movingServo.ino
@@ -6,22 +6,20 @@ int const potPin = A0;
 int potVal;
 int angle;
 
-void Setup()
-{
+void setup(){
   myServo.attach(9);
   Serial.begin(9600);
 }
 
-void loop()
-{
+void loop(){
   potVal = analogRead(potPin);
   Serial.print("potVal: ");
   Serial.print(potVal);
   
-  angle = map(potVal, 0, 1023, 0 179);
+  angle = map(potVal, 0, 1023, 0, 179);
   Serial.print(", angle: ");
   Serial.println(angle);
   
   myServo.write(angle);
   delay(15);
-{
+}
